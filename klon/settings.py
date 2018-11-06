60f80ec639ca232bbc950dfef42f62946908165f
Shun Kakinoki <shunkakinoki@gmail.com>
2018-11-06 15:56:15 +0900

    added 'klon.ap-northeast-1.elasticbeanstalk.com  ' to host

--- klon/settings.py
+++ klon/settings.py
@@ -5,7 +5,7 @@ SECRET_KEY = 's$g^n1!&9wg(2^m)ls1&8#vgl3z3_48qb-fi08l&ey&w4$d5kp'
 DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'klon.x7ee2gmxun.us-west-2.elasticbeanstalk.com',
+    'klon.ap-northeast-1.elasticbeanstalk.com',
     'klongroup.com'
 ]
 
