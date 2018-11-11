259777ab11461afaa5d57107eaec44cb779572d6
Shun Kakinoki <shunkakinoki@gmail.com>
2018-11-12 01:49:18 +0900

    added inline fields to admin

--- api/admin.py
+++ api/admin.py
@@ -2,10 +2,26 @@ from django.contrib import admin
 
 from .models import Restaurant, Food, FoodOption
 
+class FoodOptionInline(admin.TabularInline):
+    model = FoodOption
+    fk_name = "food"
+    readonly_fields = ('uuid', 'uuid_url', 'food_url')
+
+class FoodInline(admin.TabularInline):
+    model = Food
+    fk_name = "restaurant"
+    readonly_fields = ('uuid', 'uuid_url', 'restaurant_url')
+
 class RestaurantAdmin(admin.ModelAdmin):
+    inlines = [
+        FoodInline,
+    ]
     readonly_fields = ('uuid', 'uuid_url', )
 
 class FoodAdmin(admin.ModelAdmin):
+    inlines = [
+        FoodOptionInline,
+    ]
     readonly_fields = ('uuid', 'uuid_url', 'restaurant_url')
 
 class FoodOptionAdmin(admin.ModelAdmin):
