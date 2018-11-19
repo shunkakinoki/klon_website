445c0bc46485347b76647d93c7658eb9886f66b6
Shun Kakinoki <shunkakinoki@gmail.com>
2018-11-19 13:59:57 +0900

    added 404 page

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('error/', views.ErrorPageView.as_view(), name='home'),
+    path('error/', views.ErrorPageView.as_view(), name='error'),
 ]
